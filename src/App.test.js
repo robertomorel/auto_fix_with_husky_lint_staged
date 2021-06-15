8c575dc35f6b74810ffda0c2a94893f136d622f2
roberto.morel <game.developer.br@gmail.com>
2021-06-15 10:13:13 -0300

    Teste

--- src/App.test.js
+++ src/App.test.js
@@ -2,9 +2,11 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
+/*
 test('test fail', () => {
   expect(false).toBe(true);
 });
+*/
 
 test('renders Hello World!!! link', () => {
   render(<App />);
